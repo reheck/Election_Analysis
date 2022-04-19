34ccf74e607310a472d027ff043e673109306c48
Rachel Heck <heck.rachel4@gmail.com>
2022-04-18 20:47:59 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -7,31 +7,81 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
+# Initialize a total votes counter
+total_votes = 0
+# Declare Candidate Options list
+candidate_options = []
+# Declare Candidate Votes dictionary
+candidate_votes = {}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     #To do: read and analyze data
     file_reader = csv.reader(election_data)
     # Print the header row
     headers = next(file_reader)
-    print(headers)
+    
     # Print each row in the CSV file
-    #for row in file_reader:
-        #print(row)
+    for row in file_reader:
+        # Add to the total vote count
+        total_votes += 1
+        # Print the candidate name from each row
+        candidate_name = row[2]
+        #If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add it to the candidate name from each row
+            candidate_options.append(candidate_name)
+            # Begin tracking each candidate's vote  count
+            candidate_votes[candidate_name] = 0
+        # Add a vote to each candidate's count
+        candidate_votes[candidate_name] += 1
+    # Determine the percentage of votes for each candidate by looping through the counts
+    # Iterate through the candiate list
+    for candidate_name in candidate_votes:
+        #Retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # Print out each candidate's name, vote count, and percentage of votes to the terminal
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
+        #Determine the winning vote count and candidate
+        #Determine if hte vote is greater than the winning count
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percentage = vote_percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+        f"_______________________\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"_______________________\n")
+    print(winning_candidate_summary)
+
+
+
+
+        
+
+    # Using the with statement with the "w" mode we will write data to the file
+    with open(file_to_save, "w") as txt_file:
+        # Write some data to the file
+        txt_file.write("Colorado Election Analysis")
+        txt_file.write("\nCounties in the Election")
+        txt_file.write("\n_________________________")
+        # Write three counties to the file
+        txt_file.write("\nArapahoe\nDenver\nJefferson")
     
 
-# Using the with statement with the "w" mode we will write data to the file
-with open(file_to_save, "w") as txt_file:
-    # Write some data to the file
-    txt_file.write("Colorado Election Analysis")
-    txt_file.write("\nCounties in the Election")
-    txt_file.write("\n_________________________")
-    # Write three counties to the file
-    txt_file.write("\nArapahoe\nDenver\nJefferson")
-    
 
-
-# 1. Find the total number of votes cast
+# 1. Total votes  cast
 # 2. Pull a complete list of candidates who recieved votes
 # 3. Find the percentage of votes each candidate won/
 # 4. Count the total number of votes each candidate won
